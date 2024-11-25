f13fd84aa06ff9b14ac58e7e1d65e12a1ea8e44f
qoiol <hjjj1324@gmail.com>
2024-11-25 20:15:16 +0900

    admincontroller 로그인 로그아웃

---
+++ src/main/java/net/fullstack7/mooc/dto/AdminLoginDTO.java
@@ -0,0 +1,22 @@
+package net.fullstack7.mooc.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class AdminLoginDTO {
+    @NotBlank(message = "아이디를 입력해주세요.")
+    @Size(min = 5, max=15, message = "아이디 또는 비밀번호가 일치하지 않습니다.1")
+    @Pattern(regexp = "^[a-z0-9]{5,15}$", message = "아이디 또는 비밀번호가 일치하지 않습니다.2")
+    private String adminId;
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Size(min=10, max=20, message = "아이디 또는 비밀번호가 일치하지 않습니다.3")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{10,20}$", message = "아이디 또는 비밀번호가 일치하지 않습니다.4")
+    private String password;
+}
