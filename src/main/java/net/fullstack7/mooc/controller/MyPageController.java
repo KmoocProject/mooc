0e183b20786b1fd36323630e76de0731fb95e9c6
Choisarang <sarang1126611@naver.com>
2024-11-26 17:12:24 +0900

    mypage

---
+++ src/main/java/net/fullstack7/mooc/controller/MyPageController.java
@@ -0,0 +1,18 @@
+package net.fullstack7.mooc.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Log4j2
+@RequiredArgsConstructor
+@RequestMapping("/mypage")
+public class MyPageController {
+    @GetMapping("/myclass")
+    public String mypage() {
+        return "mypage/myclass";
+    }
+}
