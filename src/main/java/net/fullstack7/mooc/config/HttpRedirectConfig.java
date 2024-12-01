060b192473bc4aa58936a3cb37a6acac6e1f2877
gyeongmin <kgmksw@naver.com>
2024-12-01 17:01:55 +0900

    이모지 정상화

--- src/main/java/net/fullstack7/mooc/config/HttpRedirectConfig.java
+++ src/main/java/net/fullstack7/mooc/config/HttpRedirectConfig.java
@@ -7,17 +7,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class HttpRedirectConfig implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
-    @Override
-    public void customize(TomcatServletWebServerFactory factory) {
-        factory.addAdditionalTomcatConnectors(createHttpConnector());
-    }
+   @Override
+   public void customize(TomcatServletWebServerFactory factory) {
+       factory.addAdditionalTomcatConnectors(createHttpConnector());
+   }
 
-    private Connector createHttpConnector() {
-        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-        connector.setPort(8080);
-        connector.setScheme("http");
-        connector.setSecure(false);
-        connector.setRedirectPort(8443);
-        return connector;
-    }
+   private Connector createHttpConnector() {
+       Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+       connector.setPort(8080);
+       connector.setScheme("http");
+       connector.setSecure(false);
+       connector.setRedirectPort(8443);
+       return connector;
+   }
 }
